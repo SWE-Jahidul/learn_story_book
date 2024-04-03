23d8cdaa4b9858deab230d657695e87a976f3805
SWE-Jahidul <programmerjahid162@gmail.com>
2024-04-04 02:51:13 +0600

    Updated: Heading

--- src/stories/components/Title/Title.stories.tsx
+++ src/stories/components/Title/Title.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
 import Title from './Title';
 
-// Define Meta information for the Title component
+
 const meta: Meta = {
-  title: 'Title',
+  title: 'Heading',
   component: Title,
 };
 
@@ -11,35 +11,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const titleStyles = {
-  fontFamily: 'Arial, sans-serif',
-  marginBottom: '1rem',
-};
-
-const heading1Styles = {
-  ...titleStyles,
-  fontSize: '2rem',
-  fontWeight: 'bold',
-};
-
-const heading2Styles = {
-  ...titleStyles,
-  fontSize: '1.5rem',
-  fontWeight: 'bold',
-};
-
-const heading3Styles = {
-  ...titleStyles,
-  fontSize: '1.2rem',
-  fontWeight: 'bold',
-};
-
-// Define each story using StoryObj
 export const Heading1: Story = {
   args: {
     level: 'h1',
     children: 'Heading Level 1',
-    style: heading1Styles,
   },
 };
 
@@ -47,7 +22,6 @@ export const Heading2: Story = {
   args: {
     level: 'h2',
     children: 'Heading Level 2',
-    style: heading2Styles,
   },
 };
 
@@ -55,6 +29,21 @@ export const Heading3: Story = {
   args: {
     level: 'h3',
     children: 'Heading Level 3',
-    style: heading3Styles,
   },
+  
 };
+
+export const Heading4: Story = {
+  args: {
+    level: 'h4',
+    children: 'Heading Level 4',
+  }
+};
+
+export const Heading5: Story = {
+  args: {
+    level: 'h5',
+    children: 'Heading Level 5',
+  }
+}
+
